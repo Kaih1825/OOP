f3ce21cb798e475f59c668e2af8b6d9f3bec6f39
Kai <imatch181@gmail.com>
2025-03-24 21:28:13 +0800

    Upload Week 6-1-1

--- Week 6/CS3005302W06/TS0701/Form.cpp
+++ Week 6/CS3005302W06/TS0701/Form.cpp
@@ -1,18 +1,107 @@
+/*****************************************************************//**
+ * \file   Form.cpp
+ * \brief  Implementation of the Form class for word input, processing,
+ *         file reading, and comparison operations.
+ *
+ * \author Kai
+ * \date   2025/3/24
+ *********************************************************************/
 #include "Form.h"
+#include <iostream>
 
 using namespace std;
 
+/**
+ * Set the given word of the game
+ *
+ * \param inputWord The word user input
+ */
 void Form::SetInputWord(string inputWord) {
+	// Set the given word of the game
 	this->inputWord = inputWord;
 }
 
-void Form::ProcessInputWord() {}
+/**
+ * Converts all uppercase characters in the input word to lowercase.
+ *
+ */
+void Form::ProcessInputWord() {
+	// The variable to save the transition result
+	string tmp = "";
 
+	// Process each character in the user input
+	for (auto c : inputWord) {
+		// Converts all uppercase characters in the input word to lowercase.
+		if ((int)c < (int)'a') {
+			tmp += c - 'A' + 'a';
+		}
+		else {
+			tmp += c;
+		}
+	}
 
-void Form::SetFileName(string fileName) {
-	this->inputFStream.open("words.txt");
+	inputWord = tmp;
 }
 
-void Form::Load_CompareWord() {}
+/**
+ * Set the file name of the file that contains all the valid words.
+ *
+ * \param fileName The name of the file to open that contains all the valid words
+ */
+void Form::SetFileName(string fileName) {
+	// Set the file name of the file that contains all the valid words.
+	this->fileName = fileName;
+}
 
-void Form::PrintFoundWords() {}
+void Form::Load_CompareWord() {
+	ifstream inputFStream; // The input file stream
+
+	// Open the file
+	inputFStream.open(fileName);
+
+	// If the file
+	if (inputFStream.fail()) {
+		cout << "ERROR" << endl;
+	}
+
+	// The buffer to save each line of word
+	string buffer;
+
+	// Get the word in file one-by-one
+	while (getline(inputFStream, buffer))
+	{
+		// The variable to save user input
+		string tmp = inputWord;
+
+		// The variable to save the word is valid or not
+		bool isValid = true;
+
+		// Process each character in the buffer
+		for (auto c : buffer) {
+			// If the character is not in user input, than the word is not valid
+			if (tmp.find(c) == string::npos) {
+				isValid = false;
+				break;
+			}
+
+			// Set the index of c to '-'
+			tmp[tmp.find(c)] = '-';
+		}
+
+		// Save the result
+		if (isValid) {
+			result.push_back(buffer);
+		}
+	}
+}
+
+/**
+ * Print the words found in the function Load_CompareWord()
+ *
+ */
+void Form::PrintFoundWords() {
+	// Print the words found in the function Load_CompareWord()
+	for (auto str : result) {
+		cout << str << endl;
+	}
+}
--- Week 6/CS3005302W06/TS0701/Form.h
+++ Week 6/CS3005302W06/TS0701/Form.h
@@ -1,11 +1,22 @@
+/*****************************************************************//**
+ * \file   Form.h
+ * \brief  Declaration of the Form class for processing and comparing input words.
+ *
+ * \author Kai
+ * \date   2025/3/24
+ *********************************************************************/
 #include <string>
 #include <fstream>
+#include <vector>
+
 using namespace std;
+
 class Form
 {
 private:
-	string inputWord;
-	ifstream inputFStream;
+	string inputWord; // The word user input
+	string fileName; // The name of the file that contains all the valid words
+	vector<string> result; // The vector of all result
 public:
 	void SetInputWord(string inputWord); // set input
 	void ProcessInputWord(); // process input

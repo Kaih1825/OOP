ad998d32ee97d63f4859bbea33aa7db4615f2017
Kai <imatch181@gmail.com>
2025-04-11 17:15:47 +0800

    Upload Week 8

--- Week 8/CS1010302W08/TS0604/VecNf.cpp
+++ Week 8/CS1010302W08/TS0604/VecNf.cpp
@@ -1,103 +1,211 @@
+/*****************************************************************//**
+ * \file   VecNf.cpp
+ * \brief  Implementation of functions and classes declared in `VecNf.h`.
+ *
+ * \author Kai
+ * \date   2025/4/11
+ *********************************************************************/
 #include "VecNf.h"
 #include <iostream>
 
+ /**
+  * Construct a one-dimensional zero vector in default.
+  *
+  */
 VecNf::VecNf() {
 
 }
 
+/**
+ * Construct a cnt dimensional vector using the first cnt  numbers in dest.
+ *
+ * \param dest The input float pointer array (vector)
+ * \param cnt The size of dest
+ */
 VecNf::VecNf(float* dest, int cnt) {
+	// Save the input array into the vector
 	for (int i = 0; i < cnt; i++) {
 		this->dest.push_back(dest[i]);
 	}
 }
 
+/**
+ * Construct a new vector from a VecNf.
+ *
+ * \param rhs The source vector
+ */
 VecNf::VecNf(const VecNf& rhs) {
+	// Save the array in rhs into the vector
 	for (int i = 0; i < rhs.dest.size(); i++) {
 		dest.push_back(rhs.dest[i]);
 	}
 }
 
+/**
+ * The size of vector <dest> (dimensional)
+ *
+ * \return The size of vector <dest> (dimensional)
+ */
 int VecNf::Size()
 {
 	return dest.size();
 }
 
+/**
+ * Assign the value (vector -> vector)
+ *
+ * \param rhs The source vector
+ */
 void VecNf::operator=(VecNf rhs) {
 	cout << "ASSIGNMENT!!!" << endl;
+
+	// Clear the vector in this
 	dest.clear();
+
+	// Save the vector in rhs into vector (dest)
 	for (auto i : rhs.dest) {
 		dest.push_back(i);
 	}
 }
 
+/**
+ * Assign the value (float -> vector)
+ *
+ * \param rhs The source float
+ */
 void VecNf::operator=(float rhs) {
 	cout << "ASSIGNMENT!!!" << endl;
+
+	// Clear the vector in this
 	dest.clear();
+
+	// Save the float(rhs) into vector (dest)
 	dest.push_back(rhs);
 }
 
+/**
+ * Return the value of the certain dimension variable in the vector
+ *
+ * \param index The index of dest
+ * \return The value of the certain dimension variable in the vector
+ */
 float& VecNf::operator[](int index) {
 	return dest[index];
 }
 
+/**
+ * Implement the addition of two vectors
+ *
+ * \param rhs The right-hand-sidde of the addition
+ * \return The addition result
+ */
 VecNf VecNf::operator+(VecNf rhs) {
+	// The variable to save the result
 	vector<float> res;
 
+	// For any arithmetic operator, the two VecNf must have the same dimensional. If they are not, print "dimensions inconsistent" and return 0.
 	if (dest.size() != rhs.dest.size()) {
 		cout << "dimensions inconsistent\n";
 		res.push_back(0);
 		return VecNf(res.data(), res.size());
 	}
 
+	//  Add two vector
 	for (int i = 0; i < dest.size(); i++) {
 		res.push_back(dest[i] + rhs.dest[i]);
 	}
 
+	// Return the vector
 	return VecNf(res.data(), res.size());
 }
 
+/**
+ * Implement the subtraction of two vectors.
+ *
+ * \param rhs The right-hand-side of the subtraction
+ * \return The subtraction result
+ */
 VecNf VecNf::operator-(VecNf rhs) {
+	// The variable to save the result
 	vector<float> res;
+
+	// For any arithmetic operator, the two VecNf must have the same dimensional. If they are not, print "dimensions inconsistent" and return 0.
 	if (dest.size() != rhs.dest.size()) {
 		cout << "dimensions inconsistent\n";
 		res.push_back(0);
 		return VecNf(res.data(), res.size());
 	}
+
+	//  Minus two vector
 	for (int i = 0; i < dest.size(); i++) {
 		res.push_back(dest[i] - rhs.dest[i]);
 	}
 
+	// Return the vector
 	return VecNf(res.data(), res.size());
 }
 
+/**
+ * Implement the Inner product of two vectors. (define operator *)
+ *
+ * \param rhs The right-hand-side of the Inner product
+ * \return The Inner product result
+ */
 float VecNf::operator*(VecNf rhs) {
+	// For any arithmetic operator, the two VecNf must have the same dimensional. If they are not, print "dimensions inconsistent" and return 0.
 	if (dest.size() != rhs.dest.size()) {
 		cout << "dimensions inconsistent\n";
 		return 0;
 	}
 
+	// The variable to save the compute result
 	float sum = 0;
+
+	// Compute the Inner product
 	for (int i = 0; i < dest.size(); i++) {
 		sum += dest[i] * rhs.dest[i];
 	}
 
+	// Return the result
 	return sum;
 }
 
+/**
+ * float * vector (scalar multiplication)
+ *
+ * \param lhs The left-hand-side (float)
+ * \param rhs The right-hand-side (vector)
+ * \return The scalar multiplication of a floating-point number and a vector
+ */
 VecNf operator*(float lhs, VecNf rhs) {
+	// The variable to save the result
 	vector<float> res;
+
+	// Compute the result
 	for (int i = 0; i < rhs.dest.size(); i++) {
 		res.push_back(lhs * rhs.dest[i]);
 	}
 
+	// Return the result
 	return VecNf(res.data(), res.size());
 }
 
+/**
+ * vector * float (scalar multiplication)
+ *
+ * \param lhs The left-hand-side (vector)
+ * \param rhs The right-hand-side (float)
+ * \return The scalar multiplication of a floating-point number and a vector
+ */
 VecNf operator*(VecNf lhs, float rhs) {
+	// The variable to save the result
 	vector<float> res;
+
+	// Compute the result
 	for (int i = 0; i < lhs.dest.size(); i++) {
 		res.push_back(rhs * lhs.dest[i]);
 	}
 
+	// Return the result
 	return VecNf(res.data(), res.size());
 }
--- Week 8/CS1010302W08/TS0605/Creature.cpp
+++ Week 8/CS1010302W08/TS0605/Creature.cpp
@@ -1,34 +1,80 @@
+/*****************************************************************//**
+ * \file   Creature.cpp
+ * \brief  Implementations for Creature and Proxy class methods
+ *
+ * \author Kai
+ * \date   2025/4/11
+ *********************************************************************/
 #include "Creature.h"
 #include <string>
 #include "Diary.h"
 
+ // Static container to track all created creatures
 vector<Creature*> Creature::creatures;
+
+/**
+ * Constructor that sets name and logs creation date
+ *
+ * \param name Creature name
+ */
 Creature::Creature(string name) {
+	// Assign creature name
 	this->name = name;
+
+	// Register this creature in the global creature list
 	creatures.push_back(this);
+
+	// Log creation date using Diary's current date
 	logs.push_back("Day " + Diary::date);
 }
 
+/**
+ * Constructor that copies body attributes from base Creature
+ *
+ * \param name Creature name
+ * \param base Base Creature
+ */
 Creature::Creature(string name, Creature base) {
+	// Assign creature name
 	this->name = name;
+
+	// Copy all body attributes from the base creature
 	for (auto i : base.body) {
 		body[i.first] = i.second;
 	}
+
+	// Register this creature in the global creature list
 	creatures.push_back(this);
+
+	// Log creation date using Diary's current date
 	logs.push_back("Day " + Diary::date);
 }
 
+/**
+ * Returns a Proxy object for attribute access
+ *
+ * \param index Attribute name
+ * \return Proxy object
+ */
 Creature::Proxy Creature::operator[](string index) {
+	// Return a proxy to access the specified attribute
 	return Proxy(*this, index);
 }
 
-
+/**
+ * Assign a value to a body attribute and log the change
+ *
+ * \param value New attribute value
+ */
 void Creature::Proxy::operator=(int value)
 {
+	// Store type of change: appeared, increased, decreased
 	string type;
 	if (value == parent.body[index]) {
 		return;
 	}
+
+	// Determine the type of change based on previous value
 	if (parent.body[index] == 0) {
 		type = "appeared";
 	}
@@ -38,17 +84,33 @@ void Creature::Proxy::operator=(int value)
 	else if (parent.body[index] < value) {
 		type = "increased";
 	}
+
+	// Construct log message
 	string log = parent.name + "'s " + index + " " + type + " (" + to_string(parent.body[index]) + " -> " + to_string(value) + ").";
+
+	// Apply new value to attribute
 	parent.body[index] = value;
+
+	// Append log message to creature's log
 	parent.logs.push_back(log);
 }
 
+/**
+ * Increment a body attribute and log the change
+ *
+ * \param value Increment value
+ */
 void Creature::Proxy::operator+=(int value)
 {
+	// Store type of change: appeared, increased, decreased
 	string type;
+
+	// No change; skip logging
 	if (value == 0) {
 		return;
 	}
+
+	// Determine type based on current value and increment
 	if (parent.body[index] == 0) {
 		type = "appeared";
 	}
@@ -58,17 +120,33 @@ void Creature::Proxy::operator+=(int value)
 	else if (value > 0) {
 		type = "increased";
 	}
+
+	// Construct log message
 	string log = parent.name + "'s " + index + " " + type + " (" + to_string(parent.body[index]) + " -> " + to_string(parent.body[index] + value) + ").";
+
+	// Apply increment to attribute
 	parent.body[index] += value;
+
+	// Append log to creature's log
 	parent.logs.push_back(log);
 }
 
+/**
+ * Decrement a body attribute and log the change
+ *
+ * \param value Decrement value
+ */
 void Creature::Proxy::operator-=(int value)
 {
+	// Store type of change: appeared, increased, decreased
 	string type;
+
+	// No change; skip logging
 	if (value == 0) {
 		return;
 	}
+
+	// Determine type based on value direction
 	if (parent.body[index] == 0) {
 		type = "appeared";
 	}
@@ -78,25 +156,50 @@ void Creature::Proxy::operator-=(int value)
 	else if (value < 0) {
 		type = "increased";
 	}
+
+	// Construct log message
 	string log = parent.name + "'s " + index + " " + type + " (" + to_string(parent.body[index]) + " -> " + to_string(parent.body[index] - value) + ").";
+
+	// Apply decrement to attribute
 	parent.body[index] -= value;
+
+	// Append log to creature's log
 	parent.logs.push_back(log);
 }
 
+/**
+ * Print all non-zero body attributes to console
+ *
+ */
 void Creature::PrintStatus() {
+	// Output creature's name
 	cout << name << "'s status:\n";
+
+	// Iterate through all body attributes
 	for (auto i : body) {
+		// Only print non-zero attributes
 		if (i.second != 0) {
 			cout << i.first << " * " << i.second << endl;
 		}
 	}
+
+	// Print an empty line for spacing
 	cout << endl;
 }
 
+/**
+ * Print the creature's action log to console
+ *
+ */
 void Creature::PrintLog() {
+	// Output creature's name
 	cout << name << "'s log:\n";
+
+	// Print all log entries
 	for (auto i : logs) {
 		cout << i << endl;
 	}
+
+	// Print an empty line for spacing
 	cout << endl;
 }
